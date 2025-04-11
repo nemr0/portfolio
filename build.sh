2d9a270b52ffec83d96d17cfbe6b8a51e203152a
Omar Elnemr <omarelnemr8@gmail.com>
2025-04-11 22:21:16 +0200

    - commented build.sh validation.

--- build.sh
+++ build.sh
@@ -6,20 +6,20 @@ if [ -f "$(git rev-parse --git-dir)/shallow" ]; then
   git fetch --unshallow
 fi
 
-# If there is only one commit, proceed with build.
-if [ "$(git rev-list --count HEAD)" -lt 2 ]; then
-  echo "Not enough commits to compare. Proceeding with build."
-else
-  # Compare the last two commits to determine what files have changed.
-  CHANGED_FILES=$(git diff --name-only HEAD^ HEAD)
-  echo "Changed files: $CHANGED_FILES"
-
-  # Check if any of the changes are in the relevant directories or files.
-  if ! echo "$CHANGED_FILES" | grep -Eq '^(lib/|assets/|web/|pubspec\.yaml|build\.sh)$'; then
-    echo "No relevant changes detected. Skipping build."
-    exit 0
-  fi
-fi
+## If there is only one commit, proceed with build.
+#if [ "$(git rev-list --count HEAD)" -lt 2 ]; then
+#  echo "Not enough commits to compare. Proceeding with build."
+#else
+#  # Compare the last two commits to determine what files have changed.
+#  CHANGED_FILES=$(git diff --name-only HEAD^ HEAD)
+#  echo "Changed files: $CHANGED_FILES"
+#
+#  # Check if any of the changes are in the relevant directories or files.
+#  if ! echo "$CHANGED_FILES" | grep -Eq '^(lib/|assets/|web/|pubspec\.yaml|build\.sh)$'; then
+#    echo "No relevant changes detected. Skipping build."
+#    exit 0
+#  fi
+#fi
 
 # Clone Flutter SDK version 3.29.2 if it doesn't exist; otherwise, ensure the version is correct.
 if [ ! -d "flutter" ]; then
